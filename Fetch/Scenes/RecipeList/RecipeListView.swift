4c9797b56d096bb0fe51f671fe0991103dbeb631
Will Nixon <will_nixon@me.com>
2024-12-04 15:41:09 -0500

    move colors to own file

--- Fetch/Scenes/RecipeList/RecipeListView.swift
+++ Fetch/Scenes/RecipeList/RecipeListView.swift
@@ -1,13 +1,5 @@
 import SwiftUI
 
-enum FetchColors {
-    static var maize = Color(red: 254 / 255, green: 228 / 255, blue: 64 / 255)
-    static var richBlack = Color(red: 0 / 255, green: 16 / 255, blue: 17 / 255)
-    static var dodgerBlue = Color(red: 30 / 255, green: 150 / 255, blue: 252 / 255)
-    static var magnolia = Color(red: 245 / 255, green: 240 / 255, blue: 246 / 255)
-    static var rasberry = Color(red: 216 / 255, green: 30 / 255, blue: 91 / 255)
-}
-
 struct RecipeListView: View {
     @ObservedObject var viewModel: RecipeListViewModel
 
