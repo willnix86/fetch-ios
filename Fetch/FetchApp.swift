3836f96b4e6af9dfa9aa04bd4042f6671b7ce5f0
Will Nixon <will_nixon@me.com>
2024-12-04 10:12:52 -0500

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Will Nixon on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
